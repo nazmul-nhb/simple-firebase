7011f8545adf2f8c649fd7980a1880ab6654ceba
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-31 12:31:43 +0600

    created basic setup

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+function App() {
+    return (
+        <>
+       
+       {/* Remove From Here */}
+
+        <div className="flex gap-3 mt-3">
+        <img className=" w-[100px]" src="https://web.programming-hero.com/home/level2Home/images/logo.svg" alt="pHero" />
+        <div>
+        <h1 className="text-3xl">ViteTemplate with React-Router-DOM, Tailwind, Prop-types, React Icons and React Helmet Async <br></br>Installed Successfully</h1>
+            <p className="mt-3">Happy Coding!</p>
+        </div>
+        </div>
+            
+         {/* Remove From Here */}
+
+        </>
+    );
+}
+
+export default App;
