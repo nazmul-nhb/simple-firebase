740be5e2d8c798d83b4881b3fec145639d37fd43
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-31 23:31:28 +0600

    added github sign-in

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,14 +1,38 @@
-import { GoogleAuthProvider, getAuth, signInWithPopup } from "firebase/auth";
+import { GithubAuthProvider, GoogleAuthProvider, getAuth, signInWithPopup, signOut } from "firebase/auth";
 import app from "../../firebase/firebase.init";
 import { useState } from "react";
 
 const Login = () => {
-    const [user, setUser] = useState({})
+    const [user, setUser] = useState(null)
     const auth = getAuth(app);
-    const provider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    const githubProvider = new GithubAuthProvider()
 
     const handleGoogleSignIn = () => {
-        signInWithPopup(auth, provider)
+        signInWithPopup(auth, googleProvider)
+            .then(result => {
+                const loggedUser = result.user;
+                console.log(loggedUser);
+                setUser(loggedUser);
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
+    const handleGoogleSignOut = () => {
+        signOut(auth)
+            .then(result => {
+                console.log(result);
+                setUser(null);
+            })
+            .catch(error => {
+                console.error(error);
+            })
+    }
+
+    const handleGithubSignIn = () => {
+        signInWithPopup(auth, githubProvider)
             .then(result => {
                 const loggedUser = result.user;
                 console.log(loggedUser);
@@ -20,8 +44,16 @@ const Login = () => {
     }
 
     return (
-        <div className="flex flex-col items-center justify-center">
-            <button onClick={handleGoogleSignIn} className="btn border rounded-lg p-4">Google Login</button>
+        <div className="flex flex-col items-center justify-center gap-6">
+            {
+                user
+                    ? <button onClick={handleGoogleSignOut} className="btn border rounded-lg p-4">Log Out</button> :
+                    <>
+                        <button onClick={handleGoogleSignIn} className="btn border rounded-lg p-4">Google Login</button>
+                        <button onClick={handleGithubSignIn} className="btn border rounded-lg p-4">Github Login</button>
+                    </>
+            }
+
             {
                 user && <div className="my-8 space-y-8">
                     <h3 className="text-2xl font-medium">User: {user?.displayName}</h3>
